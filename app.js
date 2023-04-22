050d1c22c1d6a580eca05154f08eacfdbff0c454
Jakob Brunhuber <jakob.brunhuber@icloud.com>
2023-04-22 19:05:01 +0200

    Complete Logic

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { delimiter } = require("ejs");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 let items = [];
+let workItems = [];
 app.get("/", function(req, res){
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    }
-    let day = today.toLocaleDateString("en-US", options);
-
+    let day = date.getDay();
     res.render('list', {
-        day: day, //day -> day | Current Day = Weekend/Weekday
+        listTitle: day, //day -> day | Current Day = Weekend/Weekday
         items: items,
     });
 });
 
+app.get("/work", function(req, res){
+    res.render("list",{
+        listTitle: "Work List",
+        items: workItems,
+    });
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
 app.post("/", function(req, res){
-    item = req.body.newTask;
-    items.push(item);
-    res.redirect("/");
+    let item = req.body.newTask;
+    if(req.body.list === "Work List"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
 const port = 3000;
